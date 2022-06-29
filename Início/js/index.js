bd4d948256848bce56aff0733eb0964dbd42b617
EduardoViega <eduardoviega12@gmail.com>
2022-06-29 20:18:25 -0300

    Página inícial

--- Início/js/index.js
+++ Início/js/index.js
@@ -1,19 +1,15 @@
 for(let i = 1; i <= 4; i++){
     document.getElementById(`${i}`).onclick = function(){
-        mudaFundo(document.getElementById(`${i}`),document.getElementById(`id${i+i}`))
+        mudaFundo(document.getElementById(`${i}`))
     }
 }
 
 mudaFundo(document.getElementById("2"))
 
-function mudaFundo(value, fundo){
+function mudaFundo(value){
     for(let i = 1; i <= 4; i++){
         document.getElementById(`${i}`).style.backgroundColor = 'transparent';
-        document.getElementById(`id${i+i}`).style.backgroundColor = 'transparent';
     }
-    
-    fundo.style.backgroundColor = '#28345E';
 
     value.style.backgroundColor = '#28345E';
-    value.style.borderRadius = '15px';
 }
